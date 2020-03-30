e590c44966694260c8bdfbcf9fe3f058890b6b4d
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-03-30 08:29:57 +0700

    Create no1_a.java

---
+++ Modul 4/pendahuluan/no1_a.java
@@ -0,0 +1,15 @@
+
+package modul4bintangmh;
+
+public class nomor1_a {
+
+    public static void main(String[] args) {
+
+        int jml=0;
+        for(int i=1;i<=10;i++){
+           jml+=i; 
+        }
+        System.out.print("Jumlah Semua Bilangan : "+jml);
+    }
+    
+}
