a6d095718c15b6c9b957f2729d24071bacb29f60
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-03-30 08:30:16 +0700

    Create no1_b.java

---
+++ Modul 4/pendahuluan/no1_b.java
@@ -0,0 +1,9 @@
+package modul4bintangmh;
+
+public class nomor1_b {
+    public static void main(String[] args) {
+        for(double jari=1.0;jari<=2.0;jari+=0.2){
+            System.out.println("Radius ="+jari+"Luas ="+jari*Math.PI*jari);
+        }
+    }
+}
