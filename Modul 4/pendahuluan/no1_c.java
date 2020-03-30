54e13486a81344da7996ad67228b4a9a3bfc8be9
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-03-30 08:30:35 +0700

    Create no1_c.java

---
+++ Modul 4/pendahuluan/no1_c.java
@@ -0,0 +1,10 @@
+package modul4bintangmh;
+public class nomor1_c {
+    public static void main(String[] args) {
+        double r=0;
+        while(r<0.99d){
+            r=Math.random();
+            System.out.println(r);
+        }
+    }
+}
