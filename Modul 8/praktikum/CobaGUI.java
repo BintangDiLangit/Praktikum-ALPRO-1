44eda322299a3bb516807a6d0ffe86d47513ceb9
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-04-13 12:31:38 +0700

    Create CobaGUI.java

---
+++ Modul 8/praktikum/CobaGUI.java
@@ -0,0 +1,32 @@
+
+package praktikum.modul.pkg8;
+
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class CobaGUI extends JFrame {
+    Checkbox cb1=new Checkbox("Pilih A");
+    Checkbox cb2=new Checkbox("Pilih B");
+    Checkbox cb3=new Checkbox("Pilih C");
+    CobaGUI(){
+        setTitle("Program GUI Pertamaku");
+        setLocation(200,100);
+        setSize(400,350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    }
+    void objek(){
+     getContentPane().setLayout(null);
+        getContentPane().setBackground(Color.green);
+        getContentPane().add(cb1);
+        getContentPane().add(cb2);
+        getContentPane().add(cb3);
+        cb1.setBounds(30,90,150,20);
+        cb2.setBounds(30,120,150,20);
+        cb3.setBounds(30,150,150,20);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        CobaGUI ZZ=new CobaGUI();
+        ZZ.objek();
+    }
+}
