0771828fdf80a9ac6e29a1193b87e84adc2593c1
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-04-13 12:01:43 +0700

    make tp3

---
+++ Modul 6/pendahuluan/TP3.java
@@ -0,0 +1,11 @@
+package TPmodul6;
+
+public class TP3 {
+
+    public static void main(String[] args) {
+     
+        String[][] nomor={{"Andi","1"},{"Budi","2"},{"CSS","3"}};
+        System.out.println(nomor[0][0]);
+    }
+    
+}
