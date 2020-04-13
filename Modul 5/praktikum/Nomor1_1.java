bc281c6ee12a765fe520de82302ceaaedb8526a5
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-04-13 11:56:16 +0700

    Update and rename Array.java to Nomor1_1.java

--- Modul 5/praktikum/Nomor1_1.java
+++ Modul 5/praktikum/Nomor1_1.java
@@ -1,12 +1,15 @@
-package praktikummodul5;
 
-public class Array {
+package bintang.modul5;
+
+public class Nomor1_1 {
+
     public static void main(String[] args) {
         int[] data={20,10,50,30,10};
         System.out.println("Isi data anda adalah : ");
         for(int i=0;i<data.length;i++){
-            System.out.println(data[i]+" ");
-        }
-        System.out.println();
+            System.out.print(data[i]+" ");
+        }System.out.println();
     }
+    
 }
+
