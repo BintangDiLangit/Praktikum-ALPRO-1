4099b37404f6958dcdd28e030502441737867d97
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-04-13 12:26:03 +0700

    Create Tabung_1.java

---
+++ Modul 7/praktikum/Tabung_1.java
@@ -0,0 +1,10 @@
+
+package praktikummodul7;
+
+public class Tabung_1 {
+    public double tabung(int jari,int t){
+      double vt =Math.PI*Math.pow(jari, 2)*t;
+      return vt;
+        
+    }
+}
