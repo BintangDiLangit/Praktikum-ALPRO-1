b2f20e0115a5a1ea0b8d2d1ae48c996532414976
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-04-13 11:41:06 +0700

    make NewClass

---
+++ Modul 5/praktikum/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikummodul5;
+
+/**
+ *
+ * @author str5
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        int data[] = new int[5];
+        data[1]=3;
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + ", ");
+        }
+    }
+}
