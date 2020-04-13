60ccb61e093a999ae1964759a75d93358e8e39f2
Bintang Miftaqul Huda <56743346+BintangDiLangit@users.noreply.github.com>
2020-04-13 12:38:27 +0700

    Create Segitiga.java

---
+++ UTS/Segitiga.java
@@ -0,0 +1,21 @@
+
+package utsss;
+
+public class Segitiga {
+    public static void main(String[] args) {
+        
+    for(int i=1;i<10;i++){
+        for (int j = 1; j < i; j++) {
+            System.out.print("*");
+        }System.out.println();
+}
+    for(int i=0;i<7;i++){
+        for (int j = 0; j <=i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 7; j > i; j--) {
+            System.out.print("*");
+        }
+        System.out.println();
+}
+}}
